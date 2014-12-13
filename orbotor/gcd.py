d1f244a95082058e37ab70920d06cdeab9614e65
mr555 <rutroll@gmail.com>
2014-12-13 17:31:44 +0300

    adaptive collision detection loosening

--- orbotor/gcd.py
+++ orbotor/gcd.py
@@ -24,7 +24,7 @@ class GlobalCollisionDetector():
         self.loosening = False
         self.loosening_value = 0
         self.priority = []
-        self.loosening_strength = 3
+        self.loosening_strength = 0
         
     def make_priority(self, obj):
         self.orbitables.remove(obj)
@@ -35,6 +35,7 @@ class GlobalCollisionDetector():
         
     def step(self):
         self.loosening = len(self.orbitables) > 100
+        self.loosening_strength = len(self.orbitables) % 100 + 1
         if self.loosening:
             self.loosening_value = (self.loosening_value + 1) % self.loosening_strength
         checking = self.orbitables[self.loosening_value if self.loosening else 0::self.loosening_strength if self.loosening else 1]+ self.priority

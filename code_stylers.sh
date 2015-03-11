bd9e6b05e55af6c4bc2a25e07232b32b63d420db
mr555 <rutroll@gmail.com>
2015-03-11 19:59:08 +0300

    redirect it to directory

--- code_stylers.sh
+++ code_stylers.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-pylint orbotor.py ./orbotor --disable=all --enable=missing-docstring --reports=no > missing-docstrings.txt
-flake8 . --statistics --count > flake8.txt
+pylint orbotor.py ./orbotor --disable=all --enable=missing-docstring --reports=no > codestylers_logs/missing-docstrings.txt
+flake8 . --statistics --count > codestylers_logs/flake8.txt

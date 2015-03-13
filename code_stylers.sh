d36e51422d88017f6ba76593ad317c7f79bfc482
mr555 <rutroll@gmail.com>
2015-03-13 17:21:10 +0300

    switched to https://github.com/GreenSteam/pep257 for docstrings tracking

--- code_stylers.sh
+++ code_stylers.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
-pylint orbotor.py ./orbotor --disable=all --enable=missing-docstring --reports=no > codestylers_logs/missing-docstrings.txt
+if ! type "pep257" > /dev/null; then
+  echo "pep257 is not installed. pip install pep257";
+  exit;
+fi
+if ! type "flake8" > /dev/null; then
+  echo "flake8 is not installed. pip install flake8";
+  exit;
+fi
+pep257 . 2> codestylers_logs/docstrings.txt
 flake8 . --statistics --count > codestylers_logs/flake8.txt
 flake8 . --max-complexity 10 | grep C > codestylers_logs/complexity.txt

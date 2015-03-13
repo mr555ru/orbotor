f3bebfc49dfb501589d1e7c379ee85ddb36c44c1
mr555 <rutroll@gmail.com>
2015-03-13 16:39:38 +0300

    added complexity checks

--- code_stylers.sh
+++ code_stylers.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 pylint orbotor.py ./orbotor --disable=all --enable=missing-docstring --reports=no > codestylers_logs/missing-docstrings.txt
 flake8 . --statistics --count > codestylers_logs/flake8.txt
+flake8 . --max-complexity 10 | grep C > codestylers_logs/complexity.txt
